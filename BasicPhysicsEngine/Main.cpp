e52fdc00b03845f1ed5bacb33a353f8058f50559
eldar <eldar@DESKTOP-VQ8AB9J>
2018-05-01 03:08:49 +0300

    Minor additions.

--- BasicPhysicsEngine/Main.cpp
+++ BasicPhysicsEngine/Main.cpp
@@ -4,13 +4,24 @@
 #include "Constants.h"
 #include "Vector.h"
 #include "Display.h"
+#include "Object.h"
 
 #define Vector <float> FloatVector2d;
 
 
 int main ()
 	{
+	sf::RenderWindow window ( sf::VideoMode ( 1280, 720 ), "Basic Physics Engine" );
 
+	Object myObject;
+
+	while ( window.isOpen() )
+		{
+		window.clear();
+
+		myObject.updatePosition (  )
+
+		}
 
 	return 0;
 	}
--- BasicPhysicsEngine/Object.h
+++ BasicPhysicsEngine/Object.h
@@ -1,3 +1,39 @@
 #pragma once
 
+#include "Constants.h"
 
+
+class Object
+	{
+	public:
+		void updatePosition ( FloatVector2d& position, FloatVector2d& velocity, FloatVector2d& force, const float dt, const float mass )
+			{
+			velocity = velocity + ( force / mass );
+			position = position + ( velocity * dt );
+			}
+
+		FloatVector2d getPosition()
+			{
+			return position;
+			}
+
+		FloatVector2d getVelocity()
+			{
+			return velocity;
+			}
+
+		FloatVector2d getForce()
+			{
+			return force;
+			}
+
+
+	private:
+		FloatVector2d position;
+		FloatVector2d velocity;
+		FloatVector2d force;
+
+
+
+
+	};
